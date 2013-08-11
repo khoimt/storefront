ed0505e06ae994487489c65f65b2e82b84b5cc66
keq9 <keq9@keq9-PC>
2013-08-12 00:52:24 +0700

    implementing catalog

--- application/modules/storefront/controllers/IndexController.php
+++ application/modules/storefront/controllers/IndexController.php
@@ -8,6 +8,8 @@ class Storefront_IndexController extends Zend_Controller_Action
     
     public function indexAction()
     {
-        
+        $o_Catalog = new Storefront_Model_Catalog();
+        $arr = $o_Catalog->getCategoriesByParentId(0);
+        print_r($arr->toArray());
     }
 }
--- application/modules/storefront/models/Catalog.php
+++ application/modules/storefront/models/Catalog.php
@@ -4,31 +4,91 @@ class Storefront_Model_Catalog extends SF_Model_Abstract
 {
     public function getCategoriesByParentId($i_ParentId)
     {
-        
+        return $this->getResource('Category')
+                ->getCategoriesByParentId($i_ParentId);
     }
     
     public function getCategoryByIdent($sz_Ident)
     {
-        
+        return $this->getResource('Category')
+                ->getCategoryByIdent($sz_Ident);
     }
     
     public function getProductById($i_Id)
     {
-        
+        return $this->getResource('Product')
+                ->getProductById($i_Id);
     }
     
     public function getProductByIdent($sz_Ident)
     {
-        
+        return $this->getResource('Product')
+                ->getProductByIdent($sz_Ident);
     }
     
-    public function getProductByCategory($o_Category, $paged= false, 
-            $order=null, $deep=true)
-    {}
+    public function getProductsByCategory($category, $paged=false, $order=null, $deep=true)
+    {
+        if (is_string($category)) {
+            $cat = $this->getResource('Category')->getCategoryByIdent($category);
+            $categoryId = null === $cat ? 0 : $cat->categoryId;
+        } else {
+            $categoryId = $category;
+        }
+        
+        if (true === $deep) {
+            $ids = $this->getCategoryChildrenIds($categoryId, true);
+            $ids[] = $categoryId;
+            $categoryId = null === $ids ? $categoryId : $ids;
+        }
+        
+        return $this->getResource('Product')->getProductsByCategory($categoryId, $paged, $order);
+    }
     
-    public function getCategoryChildrenIds($i_CategoryId, $b_Recursive=false)
-    {}
+    /**
+     * Get a categories children categoryId values
+     *
+     * @param int     $categoryId The category to get children from
+     * @param boolean $recursive  Get the entire category branch?
+     * @return array An array of ids
+     */
+    public function getCategoryChildrenIds($categoryId, $recursive = false)
+    {
+        $categories = $this->getCategoriesByParentId($categoryId);
+        $cats = array();
+               
+        foreach ($categories as $category) {
+            $cats[] = $category->categoryId;
+            
+            if (true === $recursive) {
+                $cats = array_merge($cats, $this->getCategoryChildrenIds($category->categoryId, true));
+            }
+        }
+
+        return $cats;
+    }
     
-    public function getParentCategories($o_Category)
-    {}
+    /**
+     * Get a categories parents
+     * 
+     * @param Storefront_Resource_Category_Item $category
+     * @param boolean Append the parent to the cats array?
+     * @return array
+     */
+    public function getParentCategories($category, $appendParent = true)
+    {
+        $cats = $appendParent ? array($category) : array();
+        
+        if (0 == $category->parentId) {
+            return $cats;
+        }
+
+        $parent = $category->getParentCategory();
+        $cats[] = $parent;
+        
+        if (0 != $parent->parentId) {
+            $cats =  array_merge($cats, $this->getParentCategories($parent, false));
+        }
+        
+        return $cats;
+    }
 }
