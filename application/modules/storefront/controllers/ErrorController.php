3f3588698569ef35b2b206dcb3f7702115f54993
keq9 <keq9@keq9-PC>
2013-08-12 23:08:02 +0700

    error controller

---
+++ application/modules/storefront/controllers/ErrorController.php
@@ -0,0 +1,11 @@
+<?php
+
+class Storefront_ErrorController extends Zend_Controller_Action
+{
+    public function errorAction()
+    {
+        $o_Err = $this->_getParam('error_handler')->exception;
+        $this->view->message = $o_Err->getMessage();
+        $this->view->traceAsTring = $o_Err->getTraceAsString();
+    }
+}
