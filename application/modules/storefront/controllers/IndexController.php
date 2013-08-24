7f35a280c0ed6cea1ea752f0d2c924e8ecb86768
keq9 <keq9@tux-keq9.(none)>
2013-08-24 16:27:02 +0700

    commit

--- application/modules/storefront/controllers/IndexController.php
+++ application/modules/storefront/controllers/IndexController.php
@@ -12,4 +12,35 @@ class Storefront_IndexController extends Zend_Controller_Action
         $arr = $o_Catalog->getCategoriesByParentId(0);
         print_r($arr->toArray());
     }
+	
+	public function localeAction() 
+	{
+		$this->_helper->ViewRenderer->setNorender(true);
+		$usLocale = new Zend_Locale('auto');
+		$date = new Zend_Date('2006', Zend_Date::YEAR, $usLocale);
+		// $temp = new Zend_Measure_Temperature('100,10',
+										 // Zend_Measure::TEMPERATURE,
+										 // $usLocale);
+										 
+		echo $usLocale;
+		echo PHP_EOL;
+		echo '<br />';
+		echo $date;
+		echo PHP_EOL;
+		echo '<br />';
+		echo $temp;
+	}
+
+    public function listLocaleAction()
+    {
+		echo '<pre>';
+		var_dump(setlocale(LC_ALL, 0));
+		echo '<hr />';
+		var_dump(localeconv());
+		echo '</pre>';
+		echo '<hr />';
+		$this->_helper->ViewRenderer->setNorender(true);
+		
+        print_r(array_keys(Zend_Locale::getLocaleList()));
+    }
 }
