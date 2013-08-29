68c61d03b0cc45cc6aa34370df9fda52711101f2
keq9 <keq9@keq9-PC>
2013-08-29 07:27:03 +0700

    shopping cart

--- library/SF/Plugin/AdminContext.php
+++ library/SF/Plugin/AdminContext.php
@@ -2,7 +2,8 @@
 class SF_Plugin_AdminContext extends Zend_Controller_Plugin_Abstract
 {
     public function preDispatch(Zend_Controller_Request_Abstract $request) {
-        if ($request->getParam('isAdmin') !== false) {
+        if ($request->getParam('isAdmin', 0) === 1) {
+            echo 'abc';
             $o_Layout = Zend_Layout::getMvcInstance();
             $o_Layout->setLayout('admin');
         }
